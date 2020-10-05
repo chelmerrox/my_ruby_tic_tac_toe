e9ab1e052c95aeae9a9871ed55b77d2c2744cefc
Losalini <C.Rokocakau@gmail.com>
2020-10-06 03:13:42 +1200

    Restructure game logic in loop according to player_turn value (if it's odd or even)

--- bin/main.rb
+++ bin/main.rb
@@ -2,6 +2,8 @@
 #-------Variable declarations---------------
   position = [1,2,3,4,5,6,7,8,9]
 
+  player_turn = 1
+
 #-------------------------------------------
 
 
@@ -147,62 +149,64 @@ puts 'Here\'s the board you\'ll be using. Type in the numbers in each cell to ta
 
 puts ''
 
+display_board(position)
+
+puts ''
+
 # create a loop that keeps playing the game when there's no winner yet or when there is no draw yet
-loop do 
-   display_board(position)
+while !position.none? { |cell| cell.is_a?(Integer) }
+  if player_turn.odd?
+    puts "Make your move, #{player_one_name}"
+    player_one_choice = gets.chomp
 
-   puts ''
+    if player_one_choice == '' || !player_one_choice.match(/^[1-9]$+/)
+      player_one_choice = ask_for_number_repeatedly(position, player_one_choice).to_i
+    elsif player_one_choice != '' && player_one_choice.match(/^[1-9]$+/) && !position.include?(player_one_choice.to_i)
+      player_one_choice = ask_for_different_number(position, player_one_choice).to_i
+    else
+      player_one_choice = player_one_choice.to_i
+    end
 
-   puts "Make your move, #{player_one_name}"
-   player_one_choice = gets.chomp
+    # fill the cell in the board with player_one_symbol according to position player_one chose; update the board
+    position = position.each_with_index { |cell, index| position[index] = player_one_symbol if player_one_choice == cell }
 
-  if player_one_choice == '' || !player_one_choice.match(/^[1-9]$+/)
-    player_one_choice = ask_for_number_repeatedly(position, player_one_choice).to_i
-  elsif player_one_choice != '' && player_one_choice.match(/^[1-9]$+/) && !position.include?(player_one_choice.to_i)
-    player_one_choice = ask_for_different_number(position, player_one_choice).to_i
+    puts "player_one_choice: #{player_one_choice}"
+    puts "player_one_choice is an integer? #{player_one_choice.is_a?(Integer)}"
+    puts "Current position array: #{position}"
+
+    puts ''
+
+    player_turn += 1
+
+    display_board(position)
+
+    puts ''
   else
-    player_one_choice = player_one_choice.to_i
+    puts "Your turn, #{player_two_name}"
+    player_two_choice = gets.chomp
+
+    if player_two_choice == '' || !player_two_choice.match(/^[1-9]$+/)
+      player_two_choice = ask_for_number_repeatedly(position, player_two_choice).to_i
+    elsif player_two_choice != '' && player_two_choice.match(/^[1-9]$+/) && !position.include?(player_two_choice.to_i)
+      player_two_choice = ask_for_different_number(position, player_two_choice).to_i
+    else
+      player_two_choice = player_two_choice.to_i
+    end
+
+    position = position.each_with_index { |cell, index| position[index] = player_two_symbol if player_two_choice == cell }
+
+    puts "player_two_choice: #{player_two_choice}"
+    puts "player_two_choice is an integer? #{player_two_choice.is_a?(Integer)}"
+    puts "Current position array: #{position}"
+
+    puts ''
+
+    player_turn += 1
+
+    display_board(position)
+
+    puts ''
   end
-
-   # ask for player_one's choice again if position has been taken
-
-   # fill the cell in the board with player_one_symbol according to position player_one chose; update the board
-   position = position.each_with_index { |cell, index| position[index] = player_one_symbol if player_one_choice == cell }
-
-   puts "player_one_choice: #{player_one_choice}"
-   puts "player_one_choice is an integer? #{player_one_choice.is_a?(Integer)}"
-   puts "Current position array: #{position}"
-
-   puts ''
-
-   # print the updated board
-   display_board(position)
-
-   #break if is_a_win? || is_a_draw?; for now it breaks when the board/all position array elements are not integers
-   break if position.none? { |cell| cell.is_a?(Integer) }
-
-   puts ''
-
-   puts "Your turn, #{player_two_name}"
-   player_two_choice = gets.chomp
-
-  if player_two_choice == '' || !player_two_choice.match(/^[1-9]$+/)
-    player_two_choice = ask_for_number_repeatedly(position, player_two_choice).to_i
-  elsif player_two_choice != '' && player_two_choice.match(/^[1-9]$+/) && !position.include?(player_two_choice.to_i)
-    player_two_choice = ask_for_different_number(position, player_two_choice).to_i
-  else
-    player_two_choice = player_two_choice.to_i
-  end
-
-  # ask for player_two's choice again if position has been taken
-
-  position = position.each_with_index { |cell, index| position[index] = player_two_symbol if player_two_choice == cell }
-
-  puts "player_two_choice: #{player_two_choice}"
-  puts "player_two_choice is an integer? #{player_two_choice.is_a?(Integer)}"
-  puts "Current position array: #{position}"
-
-  puts ''
 end
 
 # if there is a winner, print message to congratulate the winner
